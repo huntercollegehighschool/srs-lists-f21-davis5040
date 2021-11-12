5765ef2c47d285be5d19c7632b47cdde4ceb8a3f
Davis Zong <93000346+davis5040@users.noreply.github.com>
2021-11-12 17:20:11 +0000

    I did the problems

--- Program2.py
+++ Program2.py
@@ -5,4 +5,12 @@ PROGRAM 2
 Define a function odd_reverse that takes a list as an argument. The function creates a new list containing only the elements in the odd indices (indices 1, 3, 5, ...) and then reverses it. The function returns that list.
 '''
 def odd_reverse(lst):
-  pass #delete this line when you start writing your code
+  newlist = []
+  for i in range(len(lst)):
+    if i % 2 == 1:
+      newlist.append(lst[i])
+  newlist.reverse()
+  return newlist
+
+
+
